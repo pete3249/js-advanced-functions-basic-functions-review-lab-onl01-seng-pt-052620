cd3c6a0e448b97f43d9569df124640809bf8f038
pete3249 <katelynrpeterson16@gmail.com>
2020-12-17 12:47:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+let wrapAdjective = function(character = "*") {
+    return function(descriptor = "special") {
+        console.log(`You are ${character}${descriptor}${character}!`)
+        return `You are ${character}${descriptor}${character}!`
+    }
+}
+
+const Calculator = {
+    add: function(num1, num2) {
+        return num1 + num2
+    },
+    subtract: function(num1, num2) {
+        return num1 - num2
+    },
+    multiply: function(num1, num2) {
+        return num1 * num2
+    },
+    divide: function(num1, num2) {
+        return num1/num2
+    }
+}
+
+function actionApplyer(integer, arrayOfFunctions) {
+    let a = integer
+
+    for (let i = 0; i < arrayOfFunctions.length; i++ ){
+        a = arrayOfFunctions[i](a)
+      }
+    return a
+}
+
+
+
+
+
+    
